d5b93b9158b1b5476f2d6069a2292b3a38125a78
DESKTOP-ADDHSM9\Dhruv <17bmiit075@gmail.com>
2024-12-14 19:07:17 +0530

    db url chnage

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskModule } from './Task/task.module';
-
+// mongodb://localhost:27017
+// mongodb+srv://admin:admin@cluster0.hwuu13w.mongodb.net/task-manager
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.hwuu13w.mongodb.net/task-manager'),
+    MongooseModule.forRoot('mongodb://localhost:27017/task-manager'),
     TaskModule
   ],
   controllers: [AppController],
